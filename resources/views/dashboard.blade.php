4211b95ab9d3c2e72ebb3ab8bf3916df3df1d80e
Muhammad Nouman <noumanbhai006@gmail.com>
2021-01-06 15:13:15 +0500

    show the data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,12 +18,14 @@
     </tr>
   </thead>
   <tbody>
+        @foreach($users as $user)
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>{{$user->name}}</td>
+      <td>{{$user->email}}</td>
+      <td>{{$user->created_at}}</td>
     </tr>
+    @endforeach
   
 
   </tbody>

b92c5810d18dac9ce3ee32136d1ebcb3e6d678aa
Muhammad Nouman <noumanbhai006@gmail.com>
2021-01-15 19:52:47 +0500

    Sidebar Brand backend
    Sidebar Brand backend

--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -1,75 +1,64 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <!-- {{ __('Dashboard') }} -->
-            <span>All<u class="text-primary">Brand</u></span>
-            <b class="float-right">Total user
-                <span class="badge badge-pill badge-info"></span>
-            </b>
+ @extends('admin.admin_dashbord')
+ @section('main')
+ <div class="container ">
+  <div class="row">
+    <div class="col-md-8">
+      <div>
 
-        </h2>
-    </x-slot>
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+          <button type="button" class="close" data-dismiss="alert">×</button> 
+          <strong>{{ $message }}</strong>
+        </div>
+        @endif
+        @if ($message = Session::get('danger'))
+        <div class="alert alert-danger alert-block">
+          <button type="button"  class="close" data-dismiss="alert">×</button> 
+          <strong>{{ $message }}</strong>
+        </div>
+        @endif
+      </div>
+      <div class="card-header"><u>All Brand</u>
+      </div>
+      <table class="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Serial No</th>
+            <th scope="col">Brand Name</th>
+            <th scope="col">Brand Image</th>
+            <th scope="col">Create At</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
 
-    <div class="py-12">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div>
+          @foreach($brands as $brand)
 
-                        @if ($message = Session::get('success'))
-                        <div class="alert alert-success alert-block">
-                            <button type="button" class="close" data-dismiss="alert">×</button> 
-                            <strong>{{ $message }}</strong>
-                        </div>
-                        @endif
-                        @if ($message = Session::get('danger'))
-                        <div class="alert alert-danger alert-block">
-                          <button type="button"  class="close" data-dismiss="alert">×</button> 
-                          <strong>{{ $message }}</strong>
-                      </div>
-                      @endif
-                  </div>
-                  <div class="card-header"><u>All Brand</u>
-                  </div>
-                  <table class="table table-hover">
-                      <thead>
-                        <tr>
-                          <th scope="col">Serial No</th>
-                          <th scope="col">Brand Name</th>
-                          <th scope="col">Brand Image</th>
-                          <th scope="col">Create At</th>
-                          <th scope="col">Action</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-
-                    @foreach($brands as $brand)
-
-                    <tr>
-                      <th scope="row">{{$loop->index+1}}</th>
-                      <td>{{$brand->brand_name}}</td>
-                      <td><img src="{{ URL::to('/') }}/images/{{ $brand->brand_image }}" class="img-thumbnail" width="50" height="50" /></td>
-                 <td>{{$brand->created_at->diffForHumans()}}</td>
-<td>
+          <tr>
+            <th scope="row">{{$loop->index+1}}</th>
+            <td>{{$brand->brand_name}}</td>
+            <td><img src="{{ URL::to('/') }}/images/{{ $brand->brand_image }}" class="img-thumbnail" width="50" height="50" /></td>
+            <td>{{$brand->created_at->diffForHumans()}}</td>
+            <td>
 
 <!-- 
     <a href="{{route('brand.edit',$brand->id)}}" class="btn btn-primary">Edit</a> 
     <a href="{{route('brand.destroy',$brand->id)}}" class="btn btn-danger">Delete</a>
-        {{ method_field('DELETE') }} -->
-                <form action="{{ route('brand.destroy', $brand->id)}}" method="post">
-                  @csrf
-                  @method('DELETE')
-         <a href="{{route('brand.edit',$brand->id)}}" class="btn btn-primary">Edit</a> 
+    {{ method_field('DELETE') }} -->
+    <form action="{{ route('brand.destroy', $brand->id)}}" method="post">
+      @csrf
+      @method('DELETE')
+      <a href="{{route('brand.edit',$brand->id)}}" class="btn btn-primary">Edit</a> 
 
-                  <button class="btn btn-danger" onclick="return confirm('Are you sure?')" type="submit">Delete</button>
-                </form>
+      <button class="btn btn-danger" onclick="return confirm('Are you sure?')" type="submit">Delete</button>
+    </form>
 
-</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $brands->links() }}
+  </td>
+</tr>
+@endforeach
+</tbody>
+</table>
+{{ $brands->links() }}
 
      <!--      @if(empty($categorys))
           @else
@@ -78,42 +67,38 @@
         </div>
         @endif -->
 
-    </div>
+      </div>
 
-    <div class="col-md-4">
+      <div class="col-md-4">
         <div class="card-header"><u>Create Brand</u></div>
         <form action="{{route('brand.store')}}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label for="brand">Brand Name</label>
-                <input type="text" class="form-control" name="brand_name" id="brand" aria-describedby="emailHelp" placeholder="Enter new Brand" >
-                @error('brand_name')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+          @csrf
+          <div class="form-group">
+            <label for="brand">Brand Name</label>
+            <input type="text" class="form-control" name="brand_name" id="brand" aria-describedby="emailHelp" placeholder="Enter new Brand" >
+            @error('brand_name')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
-            </div>
-            <!-- image -->
-            <div class="form-group">
-                <label for="image">Brand Image</label>
-                <input type="file" class="form-control" name="brand_image" id="image" >
-                @error('brand_image')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+          </div>
+          <!-- image -->
+          <div class="form-group">
+            <label for="image">Brand Image</label>
+            <input type="file" class="form-control" name="brand_image" id="image" >
+            @error('brand_image')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
-            </div>
+          </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-primary">Submit</button>
         </form>
 
+      </div>
+
     </div>
-
+  </div>
 </div>
 </div>
-</div>
-
-
-</div>
-</div>
-</x-app-layout>
-<hr>
+@endsection
 
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -20,7 +20,7 @@
                 <path class="logo-fill-white" fill="#FFF" d="M11 4v25l8 4V0z" />
               </g>
             </svg>
-            <span class="brand-name text-truncate">Sleek Dashboard</span>
+            <span class="brand-name text-truncate">Enigma Dashboard</span>
           </a>
         </div>
         <!-- begin sidebar scrollbar -->
@@ -35,20 +35,18 @@
               <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#dashboard"
               aria-expanded="false" aria-controls="dashboard">
               <i class="mdi mdi-view-dashboard-outline"></i>
-              <span class="nav-text">Dashboard</span> <b class="caret"></b>
+              <span class="nav-text">Home</span> <b class="caret"></b>
             </a>
             <ul  class="collapse show"  id="dashboard"
             data-parent="#sidebar-menu">
             <div class="sub-menu">
-              
-              
-              
-              <li  class="active" >
-                <a class="sidenav-item-link" href="index.html">
-                  <span class="nav-text">Ecommerce</span>
+      
+      <li  class="active" >
+                <a class="sidenav-item-link" href="{{route('brand.index')}}">
+                  <span class="nav-text">Brands</span>
                   
                 </a>
-              </li>
+      </li>
               
               
 
